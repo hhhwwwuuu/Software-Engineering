7532979b279690d79e6b95c31acd7e587f3f5d53
Jason <wzq0515@gmail.com>
2016-03-11 14:21:13 +0900

    first homework

---
+++ email.c
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+	char txt;
+	while(cin>>txt)
+	{
+		if (txt == '1')
+		{
+			cout<<"wzq0515@gmail.com"
+			/* code */
+		}
+		else
+			continue;
+	}
+	return 0;
+}
