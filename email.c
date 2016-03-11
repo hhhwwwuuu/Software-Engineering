231f4f8000771fbb40768dd8ddac4fe6026817ac
Jason <wzq0515@gmail.com>
2016-03-11 14:34:23 +0900

    update

--- email.c
+++ email.c
@@ -2,18 +2,26 @@
 #include <cstring>
 using namespace std;
 
+void phone(char txt)
+{
+	if (txt == '2')
+	{
+		/* code */
+		cout<<"11111111"<<endl;
+	}
+	else if (txt == '1')
+	{
+		/* code */
+		cout<<"wzq0515@gmail.com"<<endl;
+	}
+}
+
 int main()
 {
 	char txt;
 	while(cin>>txt)
 	{
-		if (txt == '1')
-		{
-			cout<<"wzq0515@gmail.com"
-			/* code */
-		}
-		else
-			continue;
+		phone(txt);
 	}
 	return 0;
 }
